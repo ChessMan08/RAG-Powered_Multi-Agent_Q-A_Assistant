ec23fbf278883f2d80a973000d7e51f0ea888c6e
Abhinav Sharma <132547321+ChessMan08@users.noreply.github.com>
2025-05-09 19:36:20 +0530

    Initial commit

--- tools/dictionary.py
+++ tools/dictionary.py
@@ -1,9 +1,5 @@
-import nltk
 from nltk.corpus import wordnet
 
-nltk.download('wordnet')
-
 def define(term: str) -> str:
-    """Return the first WordNet definition."""
     syns = wordnet.synsets(term)
     return syns[0].definition() if syns else "No definition found."
